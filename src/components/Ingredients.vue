5188aff428e4dd38971219acea4592ed501fd74b
Joshua Petrovani Miller <joshpetrovanimiller@gmail.com>
2020-11-22 21:09:24 -0500

    Added skeleton HTML for Header, Summary, and Ingredients sections.

---
+++ src/components/Ingredients.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="section-header">
+      <h3>INGREDIENTS</h3>
+      <span class="tiptext">Tap to edit</span>
+    </div>
+    <div class="ingredient-list">
+      <Ingredient></Ingredient>
+      <span class="white-cta" id="add-ing-button">+ Add Ingredient</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import Ingredient from './Ingredient';
+
+export default {
+  name: 'Ingredients',
+  components: {
+    Ingredient,
+  },
+};
+</script>
+
+<style scoped></style>
